a015531322351134386f56a8814ff2388a6091e4
Harish-Shejokar <90904311+Harish-Shejokar@users.noreply.github.com>
2023-09-11 11:05:49 +0530

    fifth

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: [FRONTEND_URL],
     method: ["GET", "PUT", "POST", "DELETE"],
     credentials:true,
 }))
