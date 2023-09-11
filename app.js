7ed3fdc203f74b6951b231bc8de256daceb5e846
Harish-Shejokar <90904311+Harish-Shejokar@users.noreply.github.com>
2023-09-11 11:18:36 +0530

    sixth

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [FRONTEND_URL],
+    origin: [process.env.FRONTEND_URL],
     method: ["GET", "PUT", "POST", "DELETE"],
     credentials:true,
 }))
